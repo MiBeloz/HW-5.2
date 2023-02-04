ad30f4fe093c0a78bef04b3af502b6ee1f1fa2a0
MiBeloz <miha_bmv_@mail.ru>
2023-02-05 01:00:17 +0600

    Fix code

--- Figure.h
+++ Figure.h
@@ -12,9 +12,9 @@ public:
 
 	int getSides() { return sides; }
 
-	virtual std::string getAllLengths() { return "Стороны=0"; };
+	virtual std::string getAllLengths() { return "Стороны=0"; }
 
-	virtual std::string getAllAngles() { return "Углы=0"; };
+	virtual std::string getAllAngles() { return "Углы=0"; }
 
 protected:
 	Figure(const int _sides) { sides = _sides; }
